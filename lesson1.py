9a471b50857ef746280736981ebbd74d4a81dc96
Taiyob Ali <taiyobali65@gmail.com>
2025-06-12 17:37:08 +0600

    "C:\Users\Taiyob Ali\Desktop\myportfolio_Linkedin,Github\class1.py\lesson1.py"
    this is the first class on python programming basic to advance journey. about python, comment, print() function and others

---
+++ lesson1.py
@@ -0,0 +1,25 @@
+
+# class 1: about python,comment,print() function and others
+"""
+python is an open source,interpreted,dynamically typed,
+high level language,object_oriented and functional,platform independent,
+embeddable and extensible,case_sensitive,portable language.
+"""
+# python interpreter don't execute comment,we just see.
+# there are two types of comment in python, single line and multiple line comment
+# it is a single line comment (#)( shortcut ctrl+/)
+"""
+it's a multi_line comment
+we need to use three quotation
+"""
+# code run shortcut at (pycharm shift+F10),(jupyter notebook shift+enter)
+# print() is an output func
+print("hello world")
+print(123456)
+print("I,m determined")
+"""
+some important thing, we must follow indentation rule,
+expression is an addition of variable and operator that return a new value,
+statement is a (anything) command of programming,
+python file extension is .py
+"""
