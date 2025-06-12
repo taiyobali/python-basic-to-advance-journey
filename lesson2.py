ef0dcdd60ba59165d3fe221ff481af6f9eeb2bca
Taiyob Ali <taiyobali65@gmail.com>
2025-06-12 19:32:58 +0600

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,56 @@
+
+# class 2: about variable
+# variable_name = value
+name = "Guido van rossum"
+print(name)
+num = 450
+print(num)
+# multiple assign 1
+asif = mixe = alex = 15000 # same
+print(asif,mixe,alex)
+print(asif+alex)
+# multiple assign 2
+pi,name,num = 3.1416,"Mike Alan",220
+print(pi,name,num)
+# var name: alpha_numeric, start with string & _ ... var, var2, _var, var_2
+# we can't use python's reserve word as variable name
+"""
+there are two types of var,local_var(in a function), global_var(all around)
+multi_word variable name: var_snake_case
+"""
+g = 9.8
+print(id(g)) # to see the memory location
+data = "basic python for mine"
+print(len(data))  # to see the length of sequence
+num1 = 450
+num2 = 332
+summ = num1+num2
+print(summ)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
