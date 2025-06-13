365cf16892d96df1243d0a947831e4f85314640b
Taiyob Ali <taiyobali65@gmail.com>
2025-06-13 17:30:59 +0600

    lesson17.py
    tuple data structure

---
+++ lesson17.py
@@ -0,0 +1,10 @@
+
+# class 17: tuple data structure
+# tuple is an immutable, ordered, allow duplicate value
+# allow all data types, represent (), tuple(())
+data = (452,63.33,True,3,2,"Hello",(878,54.65),False,2,4,{87,5412,96},[654,True,"Python"])
+print(data)
+print(type(data))
+print(isinstance(data,tuple))
+print(data[0])
+print(data[-1][2])
